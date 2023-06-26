c07f3e5c970638d84962696a57a4c02c8a0fb48c
rootc137 <pessicksanchez@gmail.com>
2023-06-26 10:28:44 +0200

    performances ameliorations..

--- assets/maugallery.js
+++ assets/maugallery.js
@@ -207,7 +207,7 @@
                                 ? '<div class="mg-prev" style="cursor:pointer;position:absolute;top:50%;left:-15px;background:white;"><</div>'
                                 : '<span style="display:none;" />'
                             }
-                            <img class="lightboxImage img-fluid" alt="Contenu de l'image affichée dans la modale au clique" width="200" height="200"/>
+                            <img class="lightboxImage img-fluid" alt="Contenu de l'image affichée dans la modale au clique" width="440" height="440"/>
                             ${
                               navigation
                                 ? '<div class="mg-next" style="cursor:pointer;position:absolute;top:50%;right:-15px;background:white;}">></div>'
